67c4a2a342714d5676ce4df6fe1bb62b8a2337b6
Eduardo Juarez <eduardo._.juarez@live.com.ar>
2023-08-09 18:59:00 -0300

    add msg in telegram

--- botTelegram.js
+++ botTelegram.js
@@ -6,12 +6,12 @@ const bot = process.env.BOT_TELEGRAM;
 process.env.CHAT_ID;
 */
 // replace the value below with the Telegram token you receive from @BotFather
-const botTelegram = () => {
+const botTelegram = (quotes) => {
     const bot = new Telegraf('6496129805:AAEDRFJ8EVK1ENu9222_eN7xQGuZTW84brk');
     const chatID = 933708324;
     const now = new Date();
     const formattedDate = now.toLocaleString();
-    bot.telegram.sendMessage(chatID, 'Se detecto un cambio en la web!!! ' + formattedDate);
+    bot.telegram.sendMessage(chatID, 'Se detecto un cambio en la web!!! ' + quotes + formattedDate);
     bot.launch();
 }
 export default botTelegram;
